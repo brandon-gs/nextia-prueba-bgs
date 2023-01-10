63dc756efffde6f3e21bbca9d7e33c5b50fa5f0c
Brandon García <41506244+brandon-gs@users.noreply.github.com>
2023-01-09 23:30:06 -0600

    1 back add required libraries (#2)
    * add morgan to log api requests
    
    * add dotenv
    
    * add cors
    
    * add cookie parser
    
    * add express-slow-down

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import * as middlewares from "./middlewares";
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL ?? "http://localhost:3000"],
+    credentials: true,
+  }),
+);
+
+app.use(middlewares.speedLimiter);
 app.get("/", (req, res) => {
   return res.json({ message: "API working" });
 });
