5f4a0951c0df20c199418c53bd4bb09c63a3f7f8
Brandon GS <brandongs180@gmail.com>
2023-01-09 23:23:26 -0600

    add cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL ?? "http://localhost:3000"],
