e20cc46f8e19cc4db78495fd376383cf868b996f
Brandon GS <brandongs180@gmail.com>
2023-01-10 00:41:55 -0600

    add jwt helpers and config

--- backend/src/api/auth/auth.helpers.ts
+++ backend/src/api/auth/auth.helpers.ts
@@ -1,4 +1,7 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { ObjectId } from "mongodb";
+import jwtConfig from "../../config/jwtConfig";
 
 export async function getHashedPassword(password: string) {
   const salt = await bcrypt.genSalt(10);
@@ -9,4 +12,14 @@ export async function getHashedPassword(password: string) {
 export async function matchPassword(hashPassword: string, password: string) {
   const matches = await bcrypt.compare(password, hashPassword);
   return matches;
+}
+
+export function createAccessToken(id: ObjectId, email: string) {
+  return jwt.sign({ _id: id, email }, jwtConfig.ACCESS_SECRET!, {
+    expiresIn: "1d",
+  });
+}
+
+export function decodeUserFromToken(token: string) {
+  return jwt.verify(token, jwtConfig.ACCESS_SECRET!);
 }
