81defa7a259ab913b9d7988cb2e27ef16817d65a
Brandon GS <brandongs180@gmail.com>
2023-01-09 23:10:51 -0600

    add morgan to log api requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   return res.json({ message: "API working" });
