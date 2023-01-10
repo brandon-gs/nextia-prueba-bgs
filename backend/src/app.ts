4571bc79df655dcd644c90b38b92910e0b3db2fb
Brandon GS <brandongs180@gmail.com>
2023-01-09 23:19:57 -0600

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL ?? "http://localhost:3000"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   return res.json({ message: "API working" });
