96f59a3ea4b746f4bcb97e0169c30aad99179f23
Brandon GS <brandongs180@gmail.com>
2023-01-10 01:19:00 -0600

    add required error message for user fields

--- backend/src/api/user/user.model.ts
+++ backend/src/api/user/user.model.ts
@@ -3,18 +3,20 @@ import * as z from "zod";
 import { db } from "../../db";
 
 export const User = z.object({
-  firstname: z.string().min(3),
-  lastname: z.string().min(3),
+  firstname: z.string({ required_error: "El nombre es requerido" }).min(3),
+  lastname: z.string({ required_error: "Los apellidos son requridos" }).min(3),
   email: z
-    .string()
+    .string({ required_error: "El correo electrónico es requerido" })
     .min(1, "El correo electrónico es requerido")
     .email("Correo electrónico inválido."),
   password: z
-    .string()
+    .string({ required_error: "La contraseña es requerida" })
     .min(1, "La contraseña es requerida")
     .min(8, "La contraseña debe tener al menos 8 carácteres")
     .max(32, "La contraseña debe tener máximo 32 carácteres"),
-  department: z.number().min(1, "El número de departamento es requerido"),
+  department: z
+    .number({ required_error: "El número de departamento es requerido" })
+    .min(1, "El número de departamento es requerido"),
 });
 
 export type User = z.infer<typeof User>;
