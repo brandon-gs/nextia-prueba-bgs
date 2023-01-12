a534d5d5cded6d2f55149732d1c12f1af42051d5
Brandon GS <brandongs180@gmail.com>
2023-01-11 20:12:45 -0600

    create invitations model

---
+++ backend/src/api/invitation/invitation.schema.ts
@@ -0,0 +1,13 @@
+import { WithId } from "mongodb";
+import * as z from "zod";
+import { db } from "../../db";
+
+export const Invitation = z.object({
+  guestName: z.string({ required_error: "El nombre es requerido" }).min(3),
+  startDate: z.date({ required_error: "La fecha de entrada es requerida" }),
+  endDate: z.date({ required_error: "La fecha de finalización es requerida" }),
+});
+
+export type Invitation = z.infer<typeof Invitation>;
+export type InvitationWithId = WithId<Invitation>;
+export const Invitations = db.collection<Invitation>("invitations");
