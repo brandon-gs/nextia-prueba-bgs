3d579a4da78b491372e2e35628728d2c20d81c7b
Brandon GS <brandongs180@gmail.com>
2023-01-12 18:43:46 -0600

    update invitation page with the new layout

--- frontend/src/pages/invitation/index.tsx
+++ frontend/src/pages/invitation/index.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import React from "react";
 import InvitationList from "../../modules/Invitation/components/InvitationList/InvitationList";
-import { Container, Grid, Typography } from "@mui/material";
-import ButtonLogout from "../../components/ButtonLogout/ButtonLogout";
+import InvitationLayout from "../../components/InvitationLayout/InvitationLayout";
 
 export default function ForgotPasswordPage() {
   return (
@@ -13,19 +12,9 @@ export default function ForgotPasswordPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container sx={{ height: "100%", py: 5 }}>
-        <Grid container>
-          <Grid item xs>
-            <Typography component="h1" variant="h4" sx={{ pb: 4 }}>
-              Lista de invitaciones
-            </Typography>
-          </Grid>
-          <Grid item>
-            <ButtonLogout />
-          </Grid>
-        </Grid>
+      <InvitationLayout title="Lista de invitaciones">
         <InvitationList />
-      </Container>
+      </InvitationLayout>
     </>
   );
 }
